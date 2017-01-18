22aed4aac5ab4362199f46b0e43d07fb94cc76b6
Junior Lisboa <juniorzk2910@gmail.com>
2017-01-18 14:37:12 -0200

    a

--- pagamento/index.php
+++ pagamento/index.php
@@ -109,7 +109,7 @@ include "../server/conecta.php";
             
           </div>-->
 
-          <span class="col l12 s12">Pagamentos realizados até 12:00 ( meio-dia ), a entrega inicia no próximo dia útil. Após este horário ficará para o próximo dia.</span>
+          <span class="col l12 s12" style="margin-top: 10vh !important; text-align: center;">Pagamentos realizados até 12:00 ( meio-dia ), a entrega inicia no próximo dia útil. Após este horário ficará para o próximo dia.</span>
 
 
           <div class="col l4 s12 menus boletao">
