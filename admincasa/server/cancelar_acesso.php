1e1a050b74d8748b51acc0dfb4f9a13d8e7dc373
Junior Lisboa <juniorzk2910@gmail.com>
2017-01-20 15:09:06 -0200

    sql

--- admincasa/server/cancelar_acesso.php
+++ admincasa/server/cancelar_acesso.php
@@ -3,7 +3,7 @@
   
   $id=$_GET['id'];
   //Consulta no banco de dados
-  $sql="UPDATE user SET pagamento = 0, pagseguro = 0, forma_pagamento = 0, data_pagamento = 0000-00-00 WHERE id= '$id'"; 
+  $sql="UPDATE user SET pagamento = 0, pagseguro = 0, maquininha = 0, forma_pagamento = 0, data_pagamento = 0000-00-00 WHERE id= '$id'"; 
   $foi = mysql_query($sql)or die (mysql_error());
   if ($foi) {
   	header("Location: ../detalhes_cliente.php?id=".$id);
