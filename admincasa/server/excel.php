58b1eff0e61ce2c43ad8f5c7af014bf61dbbcfd4
Junior Lisboa <juniorzk2910@gmail.com>
2017-01-20 18:47:21 -0200

    sql

--- admincasa/server/excel.php
+++ admincasa/server/excel.php
@@ -1,54 +1,34 @@
 <?php
-echo "teste";
-// Incluimos a classe PHPExcel
+//inclui a conexao com o banco
+include("conecta.php");
 
-include 'PHPExcel.php';
+// Procurar as informações do BD
+$SQL = "SELECT * FROM user DESC" ;
+$executa = mysql_query($SQL);
 
-// Instanciamos a classe
-$objPHPExcel = new PHPExcel();
+// Escolher o formato do arquivo
+header("Content-type: application/msexcel");
 
-// Definimos o estilo da fonte
-$objPHPExcel->getActiveSheet()->getStyle('A1')->getFont()->setBold(true);
+// Nome que arquivo será salvo
+header("Content-Disposition: attachment; filename=planilha_clientes.xls");
 
-// Criamos as colunas
-$objPHPExcel->setActiveSheetIndex(0)
-            ->setCellValue('A1', 'Listagem de Credenciamento' )
-            ->setCellValue('B1', "Nome " )
-            ->setCellValue("C1", "Sobrenome" )
-            ->setCellValue("D1", "E-mail" );
-
-// Podemos configurar diferentes larguras paras as colunas como padrão
-$objPHPExcel->getActiveSheet()->getColumnDimension('A')->setWidth(90);
-$objPHPExcel->getActiveSheet()->getColumnDimension('B')->setWidth(15);
-$objPHPExcel->getActiveSheet()->getColumnDimension('C')->setWidth(30);
-$objPHPExcel->getActiveSheet()->getColumnDimension('D')->setWidth(30);
-
-// Também podemos escolher a posição exata aonde o dado será inserido (coluna, linha, dado);
-$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(1, 2, "Fulano");
-$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(2, 2, " da Silva");
-$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(3, 2, "fulano@exemplo.com.br");
-
-// Exemplo inserindo uma segunda linha, note a diferença no segundo parâmetro
-$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(1, 3, "Beltrano");
-$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(2, 3, " da Silva Sauro");
-$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(3, 3, "beltrando@exemplo.com.br");
-
-// Podemos renomear o nome das planilha atual, lembrando que um único arquivo pode ter várias planilhas
-$objPHPExcel->getActiveSheet()->setTitle('Credenciamento para o Evento');
-
-// Cabeçalho do arquivo para ele baixar
-header('Content-Type: application/vnd.ms-excel');
-header('Content-Disposition: attachment;filename="arquivo_de_exemplo01.xls"');
-header('Cache-Control: max-age=0');
-// Se for o IE9, isso talvez seja necessário
-header('Cache-Control: max-age=1');
-
-// Acessamos o 'Writer' para poder salvar o arquivo
-$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
-
-// Salva diretamente no output, poderíamos mudar arqui para um nome de arquivo em um diretório ,caso não quisessemos jogar na tela
-$objWriter->save('php://output'); 
-
-exit;
-
+// Criar a tabela para receber os dados
+echo "<table>";
+ echo "<tr>";
+	echo "<td></td>";
+	echo "<td>Data</td>";
+	echo "<td>Nome</td>";
+	echo "<td>E-mail</td>";
+	echo "<td>Telefone</td>";
+ echo "</tr>";
+while ($r = mysql_fetch_array($executa)){
+ echo "<tr>";
+	echo "<td>".$r['id']."</td>";
+	echo "<td>" . $r["nascimento"] . "</td>";
+	echo "<td>" . $r["nome"] . "</td>";
+	echo "<td>" . $r["email"] . "</td>";
+	echo "<td>" . $r["telefone"] . "</td>";
+ echo "</tr>";
+}
+echo "</table>"; 
 ?>
