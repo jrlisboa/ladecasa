4dd7d15cba30454d1ce7b8d0db0cb87a2e939ac6
Junior Lisboa <juniorzk2910@gmail.com>
2017-01-20 19:45:19 -0200

    sql

--- admincasa/server/excel.php
+++ admincasa/server/excel.php
@@ -2,10 +2,6 @@
 //inclui a conexao com o banco
 include("conecta.php");
 
-// Procurar as informações do BD
-$SQL = "SELECT * FROM user" ;
-$executa = mysql_query($SQL);
-
 // Escolher o formato do arquivo
 header("Content-type: application/msexcel");
 
@@ -15,20 +11,25 @@ header("Content-Disposition: attachment; filename=planilha_clientes.xls");
 // Criar a tabela para receber os dados
 echo "<table>";
  echo "<tr>";
-	echo "<td></td>";
+	echo "<td>Id</td>";
 	echo "<td>Data</td>";
 	echo "<td>Nome</td>";
 	echo "<td>E-mail</td>";
 	echo "<td>Telefone</td>";
  echo "</tr>";
-/*while ($r = mysql_fetch_array($executa)){
+
+// Procurar as informações do BD
+$SQL = "SELECT * FROM user DESC" ;
+$executa = mysql_query($SQL);
+
+while ($resultado = mysql_fetch_array($executa)){
  echo "<tr>";
-	echo "<td>".$r['id']."</td>";
-	echo "<td>" . $r["nascimento"] . "</td>";
-	echo "<td>" . $r["nome"] . "</td>";
-	echo "<td>" . $r["email"] . "</td>";
-	echo "<td>" . $r["telefone"] . "</td>";
+	echo "<td>".$resultado['id']."</td>";
+	echo "<td>" . $resultado["nascimento"] . "</td>";
+	echo "<td>" . $resultado["nome"] . "</td>";
+	echo "<td>" . $resultado["email"] . "</td>";
+	echo "<td>" . $resultado["telefone"] . "</td>";
  echo "</tr>";
-}*/
+}
 echo "</table>"; 
 ?>
