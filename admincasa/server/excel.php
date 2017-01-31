51b09ae6492f62ec2473b5b45672f61e8a5bf35a
Junior Lisboa <juniorzk2910@gmail.com>
2017-01-31 06:52:56 -0200

    s

--- admincasa/server/excel.php
+++ admincasa/server/excel.php
@@ -124,10 +124,11 @@ while ($pegou = mysql_fetch_array($executa)){
 
 			$dnv = mysql_query("SELECT * FROM produto WHERE id_tipo = '$id_tipo'");
 			while ($produtos = mysql_fetch_array($dnv)) {
+				echo "<td>S</td>";
 				$id_produto = $produtos['id'];
 				$vamo = mysql_query("SELECT * FROM favorito WHERE id_user = '$id_cliente' AND id_produto = '$id_produto'");
 				$favoritos = mysql_fetch_array($vamo);
-				if (@mysql_num_rows($favoritos) == 0){
+				if (mysql_num_rows($favoritos) == 0){
 					echo "<td></td>";
 				}else{
 					echo "<td>S</td>";
