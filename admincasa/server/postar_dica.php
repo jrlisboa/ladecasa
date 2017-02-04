024edf26c06ddfc5e428b39b5d01e75a64237c09
Junior Lisboa <juniorzk2910@gmail.com>
2017-02-04 11:04:11 -0200

    s

--- admincasa/server/postar_dica.php
+++ admincasa/server/postar_dica.php
@@ -7,7 +7,7 @@ $titulo = utf8_decode($_POST['titulo']);
 $texto = utf8_decode($_POST['texto']);
 $data = date("Y/m/d", time());
 
-$foi = mysql_query("INSERT INTO post (id_user, titulo, texto, data_post) VALUES ('$id_user', '$titulo', '$texto', '$data')");
+$foi = mysql_query("INSERT INTO post (id_user, titulo, texto, data_post) VALUES ('$id_user', '$titulo', '$texto', '$data')") or die(mysql_error());
 
 if ($foi) {
 	echo "foi";
