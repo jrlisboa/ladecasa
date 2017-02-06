af1564757b7525a6f4ad806eecebd0e25b0a57c0
Junior Lisboa <juniorzk2910@gmail.com>
2017-02-06 10:37:53 -0200

    a

--- admincasa/dicas.php
+++ admincasa/dicas.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+error_reporting(0);
 if (!isset($_SESSION['usuarioID']) && $_SESSION['nivel'] != 3) { 
         session_destroy();
         header("Location: galeria.php"); exit; 
